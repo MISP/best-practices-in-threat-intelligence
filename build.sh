6c2d8af81a579d4feef59d1ecacb20c480fda73a
Alexandre Dulaunoy <a@foo.be>
2018-07-01 17:57:47 +0200

    Links fixed

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 asciidoctor book.adoc
 asciidoctor-pdf book.adoc
-
+cp book.html ../misp-website/best-practices-in-threat-intelligence.html
+cp book.pdf  ../misp-website/best-practices-in-threat-intelligence.pdf
